3088344981d78d0c9e8fb73aaa1cf6e06403d7a4
chungSungMin <taky0315@naver.com>
2024-05-27 10:43:30 +0900

    [ FIX ] K8S test

--- app/database/connection.py
+++ app/database/connection.py
@@ -16,7 +16,7 @@ igeport_db = db['igeport_db']
 
 
 
-# # 환경 변수에서 데이터베이스 연결 정보 가져오기
+# # 환경 변수에서 데이터베이스 연결 정보 가져오기 URL 수정
 # AZURE_SQL_USERNAME = os.getenv('AZURE_SQL_USERNAME')
 # AZURE_SQL_PASSWORD = os.getenv('AZURE_SQL_PASSWORD')
 # AZURE_SQL_SERVER = os.getenv('AZURE_SQL_SERVER')
