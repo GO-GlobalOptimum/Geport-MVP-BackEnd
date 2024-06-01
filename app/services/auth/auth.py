ad8a835c64dba0e009aa5c611f223d85f5dc94c8
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2024-06-02 00:08:12 +0900

    Update auth.py

--- app/services/auth/auth.py
+++ app/services/auth/auth.py
@@ -1,4 +1,3 @@
-# services/auth.py
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from pydantic import BaseModel
@@ -22,16 +21,13 @@ def get_config():
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
     return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
 
-
-# Spring에서 name 가지고 올 수 있게 만들어줘야 한다.
 def get_current_user(Authorize: AuthJWT = Depends(), credentials: HTTPAuthorizationCredentials = Security(HTTPBearer())):
     try:
         Authorize.jwt_required()
         raw_token = Authorize.get_raw_jwt()
         user_email = raw_token['email']  # 이메일 정보 추출
-        user_name = raw_token['name']    # 이름 정보 추출
-        return {"email": user_email, "name": user_name}
+        return {"email": user_email}
     except KeyError:
-        raise HTTPException(status_code=400, detail="Email or name not found in token")
+        raise HTTPException(status_code=400, detail="Email not found in token")
     except AuthJWTException as e:
         raise HTTPException(status_code=401, detail=str(e))
