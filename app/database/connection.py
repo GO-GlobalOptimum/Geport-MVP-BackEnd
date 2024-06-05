6516c39744f47a9be2f23540e1cf4a6c95d16db8
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2024-06-06 03:00:40 +0900

    Update connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
+# MongoDB 설정
 mongodb_url = os.getenv("MONGODB_URL")
 client = MongoClient(mongodb_url)
 db = client["admin"]
@@ -14,26 +15,39 @@ igeport_user_baseInfo_collection = db['igeport_users_info']
 geport_db = db['geport_db']
 igeport_db = db['igeport_db']
 
-
-
-# # 환경 변수에서 데이터베이스 연결 정보 가져오기 URL 수정
+# 환경 변수에서 데이터베이스 연결 정보 가져오기 URL 수정
 # AZURE_SQL_USERNAME = os.getenv('AZURE_SQL_USERNAME')
 # AZURE_SQL_PASSWORD = os.getenv('AZURE_SQL_PASSWORD')
 # AZURE_SQL_SERVER = os.getenv('AZURE_SQL_SERVER')
 # AZURE_SQL_DATABASE = os.getenv('AZURE_SQL_DATABASE')
 
-DB_URL = os.environ.get('MYSQL_URL')
+DB_URL_READ = os.environ.get('MYSQL_URL_READ')
+DB_URL_WRITE = os.environ.get('MYSQL_URL_WRITE')
+
 # DB_URL = f'mysql+pymysql://{AZURE_SQL_USERNAME}:{AZURE_SQL_PASSWORD}@{AZURE_SQL_SERVER}:3306/{AZURE_SQL_DATABASE}'
 
-# SQLAlchemy 엔진 생성
-engine = create_engine(DB_URL, pool_recycle=500)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# 읽기 전용 엔진 및 세션 생성
+read_engine = create_engine(DB_URL_READ, pool_recycle=500)
+ReadSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=read_engine)
 
+# 쓰기 전용 엔진 및 세션 생성
+write_engine = create_engine(DB_URL_WRITE, pool_recycle=500)
+WriteSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=write_engine)
+
+# SQLAlchemy 기본 클래스 생성
 Base = declarative_base()
 
-# 데이터베이스 세션 생성 함수
-def get_db():
-    db = SessionLocal()
+# 읽기 전용 데이터베이스 세션 생성 함수
+def get_read_db():
+    db = ReadSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# 쓰기 전용 데이터베이스 세션 생성 함수
+def get_write_db():
+    db = WriteSessionLocal()
     try:
         yield db
     finally:
